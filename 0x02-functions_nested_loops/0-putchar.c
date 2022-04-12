2a22b27351af98681cf065381085391e9ccab3bb
TemesgenFufa <temesgenfufa70@gmail.com>
2022-04-12 11:53:00 +0000

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,11 +11,11 @@
 int main(void)
 {
 	int n = 0;
-	char str_hbt[9] = "_putchar";
+	char str[] = "_putchar";
 
 	while (n < 9)
 	{
-		_putchar(str_hbt[n]);
+		_putchar(str[n]);
 		n += 1;
 	}
 	_putchar('\n');
